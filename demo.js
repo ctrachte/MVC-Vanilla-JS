a2ffc44938e19208dccead5118e26dc9234569e3
Caleb Trachte <cetrachte@hotmail.com>
2019-10-14 09:44:37 -0500

    add displayTodos function

--- demo.js
+++ demo.js
@@ -79,6 +79,14 @@ class View {
       // Append the title, form, and todo list to the app
       this.app.append(this.title, this.form, this.todoList)
   }
+  // below are PRIVATE helpers to be used only inside this class
+  get _todoText() {
+    return this.input.value
+  }
+
+  _resetInput() {
+    this.input.value = ''
+  }
   // Create an element with an optional CSS class
   createElement(tag, className) {
     const element = document.createElement(tag)
@@ -92,6 +100,55 @@ class View {
     const element = document.querySelector(selector)
 
     return element
+  }
+  // The displayTodos method will create the ul and lis that the todo list consists of, and display them.
+  // Every time a todo is changed, added, or removes, the displayTodos method will be called again with the todos from the model,
+  //  resetting the list and redisplaying them. This will keep the view in sync with the model state.
+  displayTodos (todos) {
+    // Delete all nodes
+    while (this.todoList.firstChild) {
+      this.todoList.removeChild(this.todoList.firstChild)
+    }
+    // Show default message
+    if (todos.length === 0) {
+      const p = this.createElement('p')
+      p.textContent = 'Nothing to do! Add a task?'
+      this.todoList.append(p)
+    } else {
+      // loop through the todos and display a checkbox, span, and delete button for every existing todo.
+        todos.forEach(todo => {
+          const li = this.createElement('li')
+          li.id = todo.id
+
+          // Each todo item will have a checkbox you can toggle
+          const checkbox = this.createElement('input')
+          checkbox.type = 'checkbox'
+          checkbox.checked = todo.complete
+
+          // The todo item text will be in a contenteditable span
+          const span = this.createElement('span')
+          span.contentEditable = true
+          span.classList.add('editable')
+
+          // If the todo is complete, it will have a strikethrough
+          if (todo.complete) {
+            const strike = this.createElement('s')
+            strike.textContent = todo.text
+            span.append(strike)
+          } else {
+            // Otherwise just display the text
+            span.textContent = todo.text
+          }
+
+          // The todos will also have a delete button
+          const deleteButton = this.createElement('button', 'delete')
+          deleteButton.textContent = 'Delete'
+          li.append(checkbox, span, deleteButton)
+
+          // Append nodes to the todo list
+          this.todoList.append(li)
+        })
+    }
   }
 }
 
