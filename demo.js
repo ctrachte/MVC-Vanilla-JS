befb677facf28b9ee1a288cab927d9508ab063ed
Caleb Trachte <cetrachte@hotmail.com>
2019-10-14 09:52:09 -0500

    add basic helper functions for controller class

--- demo.js
+++ demo.js
@@ -156,6 +156,13 @@ class Controller {
   constructor(model, view) {
     this.model = model
     this.view = view
+
+    // Display initial todos
+    this.onTodoListChanged(this.model.todos)
+  }
+
+  onTodoListChanged = todos => {
+    this.view.displayTodos(todos)
   }
 }
 
