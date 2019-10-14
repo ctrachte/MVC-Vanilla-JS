511b116dacaea4b1dc52b83cfc6c35f782750b0e
Caleb Trachte <cetrachte@hotmail.com>
2019-10-14 09:30:02 -0500

    add helper functions to create and add

--- demo.js
+++ demo.js
@@ -48,8 +48,27 @@ class Model {
     }
 }
 
+
+// create a VIEW by manipulating the DOM - document object model
 class View {
-  constructor() {}
+  constructor() {
+
+  }
+
+  // Create an element with an optional CSS class
+  createElement(tag, className) {
+    const element = document.createElement(tag)
+    if (className) element.classList.add(className)
+
+    return element
+  }
+
+  // Retrieve an element from the DOM
+  getElement(selector) {
+    const element = document.querySelector(selector)
+
+    return element
+  }
 }
 
 class Controller {
